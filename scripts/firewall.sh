096a46790af1034b6c4c56b51930149b754ebe05
Antoine Habran <antoine.habran@gmail.com>
2013-10-19 23:31:17 +0200

    tagada

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -9,6 +9,6 @@ host=eth0
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -t nat -A PREROUTING -p tcp -i $host --dport 80 -j DNAT --to-destination $apache:80
-iptables -t nat -A PREROUTING -p tcp -i $host --dport 443 -j DNAT --to-destionation $apache:433
+iptables -t nat -A PREROUTING -p tcp -i $host --dport 443 -j DNAT --to-destination $apache:433
 iptables -t nat -A PREROUTING -p tcp -i $host --dport 25 -j DNAT --to-destination $mail:25
-iptabltes -t nat -A PREROUTING -p tcp -i $host --dport 53 -j DNAT --to-destionation $dns:53
+iptables -t nat -A PREROUTING -p tcp -i $host --dport 53 -j DNAT --to-destination $dns:53
